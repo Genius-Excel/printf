30111ab98440dc2c30ff4f9ab27aa2ace85afa32
Genius-Excel <geniusexcel9@gmail.com>
2023-08-20 05:16:26 -0700

    Print to stdout in specified format

--- 0-print_format.c
+++ 0-print_format.c
@@ -5,10 +5,11 @@
 * _printf - this function takes number of consequential arguments
 * and prints the value of the argument recived to stndard output
 * it orks just like the printf function in the stadard library.
-* @format: memeber parameter ofthe function that specifies the format
-* of the string array passed to it and how it should be printed in standard
-* output.
-* Return: the value of the passed arguments and the number of characers printed.
+* @format: memeber parameter of the function that specifies the format
+* of the string array passed to it and how it should be
+* printed to standard output.
+* Return: the value of the passed arguments and
+* the number of characers printed.
 */
 
 int _printf(const char *format, ...);
@@ -68,6 +69,35 @@ int _printf(const char *format, ...)
 
 				char_print_counter += arr_len;
 			}
+			else if (format[i] == 'd')
+			{
+				int integer_val = va_arg(format_specifier_args, int);
+
+				_print_integer(integer_val);
+				if (integer_val < 0)
+				{
+					char_print_counter += 2;
+				}
+				else
+				{
+					char_print_counter += 1;
+				}
+			}
+			else if (format[i] == 'i')
+			{
+				int decimal_val = va_arg(format_specifier_args, int);
+
+				_print_integer(decimal_val);
+				/**checks if number is negative to count the '-' sign **/
+				if (decimal_val < 0)
+				{
+					char_print_counter += 2;
+				}
+				else
+				{
+					char_print_counter += 1;
+				}
+			}
 		}
 		else
 		{
@@ -79,4 +109,28 @@ int _printf(const char *format, ...)
 	va_end(format_specifier_args);
 
 	return (char_print_counter);
+}
+
+
+/**
+* _print_integer - this function prints the integer value
+* passed as argument
+* @integer: member parameter for the integer
+* Return: void
+*/
+
+void _print_integer(int integer)
+{
+	if (integer < 0)
+	{
+		_putchar('-');
+		integer = -(integer);
+	}
+
+	if (integer / 10)
+	{
+		_print_integer(integer / 10);
+	}
+
+	_putchar(integer % 10 + '0');
 }
