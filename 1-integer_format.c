79737b1899cf2d7f25c663a3a1ea0969a57ab4d8
Genius-Excel <geniusexcel9@gmail.com>
2023-08-19 17:35:36 -0700

    Prints integers in their specified format

--- 1-integer_format.c
+++ 1-integer_format.c
@@ -70,3 +70,27 @@ int _printf(const char *format, ...)
 
 	return (char_print_counter);
 }
+
+
+/**
+* _print_integer - this function prints an integer to the standard output
+* @integer: integer passed as argument to be printed
+* Return: void
+*/
+
+void _print_integer(int integer)
+{
+	if (integer < 0)
+	{
+		_putchar('-');
+		integer = -(integer);
+	}
+
+	if (integer / 10)
+	{
+		_print_integer(integer / 10);
+	}
+
+	_putchar(integer % 10 + '0');
+
+}
