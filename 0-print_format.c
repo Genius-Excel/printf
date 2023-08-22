5404a5141fe5a2fc59b32620d8bb644155c096f1
BERTLEY COLLINS <collinsbertley@gmail.com>
2023-08-22 04:40:43 -0700

    corrections for 0-print_format.c

--- 0-print_format.c
+++ 0-print_format.c
@@ -29,102 +29,38 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
-	for (i = 0; format[i] != '\0'; i++)
+for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '\0')
+		if (format[i] != '%')
 		{
-			break;
-		}
-		else if (format[i] == '%')
-		{
-			i += 1;
-
-			if (format[i] == '%')
-			{
-				_putchar('%');
-				char_print_counter += 1;
-			}
-			else if (format[i] == 'c')
-			{
-				char character_val = va_arg(format_specifier_args, int);
-
-				if (character_val)
-				{
-					_putchar(character_val);
-					char_print_counter += 1;
-				}
-				else
-				{
-					exit(1);
-				}
-			}
-			else if (format[i] == 's')
-			{
-				char *string_arr = va_arg(format_specifier_args, char *);
-
-				int str_idx = 0;
-
-				int arr_len = 0;
-
-				for (str_idx = 0; string_arr[str_idx] != '\0'; str_idx++)
-				{
-					_putchar(string_arr[str_idx]);
-				}
-
-				/** counts the total number of characters printed fron the string array.**/
-
-				while (*string_arr != '\0')
-				{
-					arr_len += 1;
-					string_arr += 1;
-				}
-
-				char_print_counter += arr_len;
-			}
-			else if (format[i] == 'd')
-			{
-				int dec_len = 0;
-				int integer_val = va_arg(format_specifier_args, int);
-
-				_print_integer(integer_val);
-				if (!(integer_val))
-				{
-					return (-1);
-				}
-
-				dec_len = _integer_length(integer_val);
-				char_print_counter += dec_len;
-
-				_print_integer(integer_val);
-			}
-			else if (format[i] == 'i')
-			{
-				int int_len = 0;
-				int decimal_val = va_arg(format_specifier_args, int);
-
-				if (!(decimal_val))
-				{
-					return (-1);
-				}
-
-				int_len = _integer_length(decimal_val);
-				char_print_counter += int_len;
-
-				_print_integer(decimal_val);
-			}
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			_putchar(format[i]);
-			char_print_counter += 1;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
 
-	va_end(format_specifier_args);
+	print_buffer(buffer, &buff_ind);
 
-	return (char_print_counter);
+	va_end(list);
+
+	return (printed_chars);
 }
-
 
 /**
 * _integer_length - this function is a supplement to
@@ -157,51 +93,15 @@ int _integer_length(int integer)
 }
 
 /**
-* _print_integer - this function prints the integer value
-* passed as argument
-* @integer: member parameter for the integer
-* Return: void
-*/
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
 
-void _print_integer(int integer)
+void print_buffer(char buffer[], int *buff_ind)
 {
-	int int_idx = 0;
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-	int integer_arr_len = 0;
-
-	char *integer_arr;
-
-	integer_arr_len = _integer_length(integer);
-
-	if (integer == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	integer_arr = (char *)malloc(integer_arr_len + 1);
-
-	if (integer_arr == NULL)
-	{
-		return;
-	}
-
-	if (integer < 0)
-	{
-		_putchar('-');
-		integer = -(integer);
-	}
-
-	for (int_idx = (integer_arr_len - 1); int_idx >= 0; int_idx--)
-	{
-		integer_arr[int_idx] = (integer % 10) + '0';
-		integer = integer / 10;
-	}
-
-	for (int_idx = 0; int_idx < integer_arr_len; int_idx++)
-	{
-		_putchar(integer_arr[int_idx]);
-	}
-
-	free(integer_arr);
+	*buff_ind = 0;
 }
